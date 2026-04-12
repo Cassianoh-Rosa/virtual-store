87ef1197e9c08fd079b20a612c0fde9d47e168ec
Cassianoh-Rosa <cassianoh439@gmail.com>
2026-04-12 00:58:41 -0300

    fix

--- src/script/products.js
+++ src/script/products.js
@@ -1,56 +1,56 @@
 export const products = [
     {
         name:'Positivo Laptop',
-        img:"/src/images/laptop.png",
+        img:"./src/images/laptop.png",
         price: 700,
         category:'computers',
         id: 1
     },
     {
         name:'Motorola Cellphone',
-        img:"/src/images/cellphone.avif",
+        img:"./src/images/cellphone.avif",
         price: 600,
         category:'cellphones',
         id: 2
     },
     {
         name:'Case',
-        img:"/src/images/case.png",
+        img:"./src/images/case.png",
         price: 10,
         category:'cellphones',
         id: 3
     },
     {
         name:'Wristwatch',
-        img:"/src/images/watch.png",
+        img:"./src/images/watch.png",
         price: 80,
         category:'acessories',
         id: 4
     },
     {
         name:'Camera',
-        img:"/src/images/camera.png",
+        img:"./src/images/camera.png",
         price: 550,
         category:'cameras',
         id: 5
     },
     {
         name:'Xaiomi Cellphone',
-        img:'/src/images/cellphone-xaiomi.webp',
+        img:'./src/images/cellphone-xaiomi.webp',
         price: 550,
         category:'cellphones',
         id:6
     },
     {
         name:'Iphone 15',
-        img:'/src/images/cellphone-apple.png',
+        img:'./src/images/cellphone-apple.png',
         price: 700,
         category:'cellphones',
         id:7
     },
     {
         name:'Thinkpad laptop',
-        img:'/src/images/laptop2.png',
+        img:'./src/images/laptop2.png',
         price: 600,
         category:'computers',
         id:8
--- src/script/script.js
+++ src/script/script.js
@@ -1,26 +1,23 @@
-
-import {products} from "./products.js"
+import { products } from "./products.js"
 
 document.querySelector('#categorias').addEventListener('change', updateCategory)
 
-const cart = []
+let cart = JSON.parse(localStorage.getItem('cart')) || []
 
 function updateCategory(){
     const valor_categoria = document.querySelector('#categorias').value
+
     if(valor_categoria === 'All'){
         updateList(products)
-    } else{
-    const productsInCategoria = products.filter((obj)=>{
-        if(obj.category === valor_categoria){
-            return obj
-        }
-    })
-     updateList(productsInCategoria)
-}
+    } else {
+        const productsInCategoria = products.filter(obj => obj.category === valor_categoria)
+        updateList(productsInCategoria)
+    }
 }
 
 function updateList(array){
     document.querySelector('#products').innerHTML = ''
+
     array.forEach(obj => {
         let container = document.createElement('div')
         container.classList.add('card')
@@ -36,31 +33,40 @@ function updateList(array){
         
         let button_add_cart = document.createElement('button')
         button_add_cart.innerText = 'Add'
-        button_add_cart.addEventListener('click',()=>{
-            cart.push(
-                {id: obj.id}
-            )
+        button_add_cart.addEventListener('click', () => {
+
+            const existingProduct = cart.find(item => item.id === obj.id)
+
+            if (existingProduct) {
+                existingProduct.quantity++
+            } else {
+                cart.push({ id: obj.id, quantity: 1 })
+            }
+
             updateCart()
             updateTot()
+            saveCart()
         })
+
         const price = document.createElement('p')
-        price.innerHTML =`${obj.price}$` 
+        price.innerHTML = `${obj.price}$`
 
         container.appendChild(tittle)
         container.appendChild(image)
         container_info.appendChild(button_add_cart)
         container_info.appendChild(price)
         container.appendChild(container_info)
+
         document.querySelector('#products').appendChild(container)
-    });
+    })
 }
 
 function updateCart(){
     let cart_list = document.querySelector('#List_cart')
     cart_list.innerHTML = ''
 
-        cart.forEach((obj)=>{
-        const product = products.find((p)=> p.id === obj.id)
+    cart.forEach((obj) => {
+        const product = products.find(p => p.id === obj.id)
 
         const container = document.createElement('div')
 
@@ -70,42 +76,65 @@ function updateCart(){
         const image = document.createElement('img')
         image.src = product.img
 
+        const quantity = obj.quantity
+        const qtyText = document.createElement('p')
+        qtyText.innerText = `Qtd: ${quantity}`  
+
         const container_info = document.createElement('div')
 
         const remove_button = document.createElement('button')
-        remove_button.innerHTML = 'Delete'
+        remove_button.innerText = 'Delete'
         remove_button.addEventListener('click', () => {
-        const index = cart.findIndex((item) => item.id === obj.id);
-        cart.splice(index, 1);
-        updateCart();
-        updateTot()
-    });
-
-    const price = document.createElement('p')
-    price.innerText = `${product.price}$`
-
-    container.appendChild(tittle)
-    container.appendChild(image)
-    container_info.appendChild(price)
-    container_info.appendChild(remove_button)
-    container.appendChild(container_info)
-
-    document.querySelector('#List_cart').appendChild(container)
-    })
-}
-function updateTot(){
-    const productsInCart = cart.map((obj)=>{
-      const sla = products.find((produto)=>{
-            if(produto.id === obj.id){
-                return produto
+            if (obj.quantity > 1) {
+                obj.quantity--
+            } else {
+                const index = cart.findIndex(item => item.id === obj.id)
+                cart.splice(index, 1)
             }
+
+            updateCart()
+            updateTot()
+            saveCart()
         })
-        return sla
-    })
-    // só falta percorrer o array "productsInCart" somando todos os preços dos objetos nele
-    const soma = productsInCart.reduce((tot,obj)=>{
-      return tot+= obj.price
-    },0)
+
+        const price = document.createElement('p')
+        price.innerText = `${product.price}$`
+
+        container.appendChild(tittle)
+        container.appendChild(image)
+        container_info.appendChild(price)
+        container_info.appendChild(remove_button)
+        container.appendChild(container_info)
+        container.appendChild(qtyText)
+
+        cart_list.appendChild(container)
+    }) 
+}
+
+function updateTot(){
+    const soma = cart.reduce((total, item) => {
+        const product = products.find(p => p.id === item.id)
+        return total + (product.price * item.quantity)
+    }, 0)
+
     document.querySelector('#tot').innerText = soma
 }
+
+document.querySelector('#search_input').addEventListener('input', handleSearch)
+
+function handleSearch(){
+    const searchValue = document.querySelector('#search_input').value.toLowerCase()
+
+    const filteredProducts = products.filter(product =>
+        product.name.toLowerCase().includes(searchValue)
+    )
+
+    updateList(filteredProducts)
+}
+function saveCart(){
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
 updateList(products)
+updateCart()
+updateTot()
